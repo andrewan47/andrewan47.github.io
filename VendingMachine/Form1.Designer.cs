c29bc3d1acada4a4524168ef13bf57037da0c06b
Andrew Alarcon <andrewan47@gmail.com>
2017-11-30 00:21:47 -0800

    Vending Machine Project
    This was a simple Vending Machine project in C#. Has a simple UI and dispenses change and item selected if there is any available.
    Added a zip file as well to easily test the project.

---
+++ VendingMachine/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace VendingMachine
+{
+    partial class VendingChoice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DollarBox = new System.Windows.Forms.PictureBox();
+            this.DimeBox = new System.Windows.Forms.PictureBox();
+            this.QuarterBox = new System.Windows.Forms.PictureBox();
+            this.NickelBox = new System.Windows.Forms.PictureBox();
+            this.Item2Button = new System.Windows.Forms.RadioButton();
+            this.Item1Button = new System.Windows.Forms.RadioButton();
+            this.Item3Button = new System.Windows.Forms.RadioButton();
+            this.Item4Button = new System.Windows.Forms.RadioButton();
+            this.Display = new System.Windows.Forms.TextBox();
+            this.VendingGroup = new System.Windows.Forms.GroupBox();
+            this.PurchaseBox = new System.Windows.Forms.GroupBox();
+            this.ReturnCash = new System.Windows.Forms.Button();
+            this.LoadButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DollarBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DimeBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.QuarterBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NickelBox)).BeginInit();
+            this.VendingGroup.SuspendLayout();
+            this.PurchaseBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // DollarBox
+            // 
+            this.DollarBox.Image = global::VendingMachine.Properties.Resources.dollarWashington;
+            this.DollarBox.Location = new System.Drawing.Point(6, 22);
+            this.DollarBox.Name = "DollarBox";
+            this.DollarBox.Size = new System.Drawing.Size(125, 88);
+            this.DollarBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.DollarBox.TabIndex = 3;
+            this.DollarBox.TabStop = false;
+            this.DollarBox.Click += new System.EventHandler(this.DollarBox_Click);
+            // 
+            // DimeBox
+            // 
+            this.DimeBox.Image = global::VendingMachine.Properties.Resources.Dime_Obverse_13;
+            this.DimeBox.Location = new System.Drawing.Point(19, 118);
+            this.DimeBox.Name = "DimeBox";
+            this.DimeBox.Size = new System.Drawing.Size(100, 90);
+            this.DimeBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.DimeBox.TabIndex = 2;
+            this.DimeBox.TabStop = false;
+            this.DimeBox.Click += new System.EventHandler(this.DimeBox_Click);
+            // 
+            // QuarterBox
+            // 
+            this.QuarterBox.Image = global::VendingMachine.Properties.Resources.quarter_obverse;
+            this.QuarterBox.Location = new System.Drawing.Point(154, 22);
+            this.QuarterBox.Name = "QuarterBox";
+            this.QuarterBox.Size = new System.Drawing.Size(100, 90);
+            this.QuarterBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.QuarterBox.TabIndex = 1;
+            this.QuarterBox.TabStop = false;
+            this.QuarterBox.Click += new System.EventHandler(this.QuarterBox_Click);
+            // 
+            // NickelBox
+            // 
+            this.NickelBox.Image = global::VendingMachine.Properties.Resources.Jefferson_Nickel_Unc_Obv;
+            this.NickelBox.Location = new System.Drawing.Point(154, 118);
+            this.NickelBox.Name = "NickelBox";
+            this.NickelBox.Size = new System.Drawing.Size(100, 90);
+            this.NickelBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.NickelBox.TabIndex = 0;
+            this.NickelBox.TabStop = false;
+            this.NickelBox.Click += new System.EventHandler(this.NickelBox_Click);
+            // 
+            // Item2Button
+            // 
+            this.Item2Button.AutoSize = true;
+            this.Item2Button.Location = new System.Drawing.Point(6, 71);
+            this.Item2Button.Name = "Item2Button";
+            this.Item2Button.Size = new System.Drawing.Size(67, 17);
+            this.Item2Button.TabIndex = 4;
+            this.Item2Button.Text = "Crackers";
+            this.Item2Button.UseVisualStyleBackColor = true;
+            this.Item2Button.CheckedChanged += new System.EventHandler(this.Item2Button_CheckedChanged);
+            // 
+            // Item1Button
+            // 
+            this.Item1Button.AutoSize = true;
+            this.Item1Button.Location = new System.Drawing.Point(6, 37);
+            this.Item1Button.Name = "Item1Button";
+            this.Item1Button.Size = new System.Drawing.Size(64, 17);
+            this.Item1Button.TabIndex = 5;
+            this.Item1Button.Text = "Peanuts";
+            this.Item1Button.UseVisualStyleBackColor = true;
+            this.Item1Button.CheckedChanged += new System.EventHandler(this.Item1Button_CheckedChanged);
+            // 
+            // Item3Button
+            // 
+            this.Item3Button.AutoSize = true;
+            this.Item3Button.Location = new System.Drawing.Point(6, 108);
+            this.Item3Button.Name = "Item3Button";
+            this.Item3Button.Size = new System.Drawing.Size(74, 17);
+            this.Item3Button.TabIndex = 6;
+            this.Item3Button.Text = "Candy Bar";
+            this.Item3Button.UseVisualStyleBackColor = true;
+            this.Item3Button.CheckedChanged += new System.EventHandler(this.Item3Button_CheckedChanged);
+            // 
+            // Item4Button
+            // 
+            this.Item4Button.AutoSize = true;
+            this.Item4Button.Location = new System.Drawing.Point(6, 143);
+            this.Item4Button.Name = "Item4Button";
+            this.Item4Button.Size = new System.Drawing.Size(63, 17);
+            this.Item4Button.TabIndex = 7;
+            this.Item4Button.Text = "Cookies";
+            this.Item4Button.UseVisualStyleBackColor = true;
+            this.Item4Button.CheckedChanged += new System.EventHandler(this.Item4Button_CheckedChanged);
+            // 
+            // Display
+            // 
+            this.Display.Location = new System.Drawing.Point(112, 12);
+            this.Display.Name = "Display";
+            this.Display.ReadOnly = true;
+            this.Display.Size = new System.Drawing.Size(166, 20);
+            this.Display.TabIndex = 8;
+            // 
+            // VendingGroup
+            // 
+            this.VendingGroup.Controls.Add(this.Item1Button);
+            this.VendingGroup.Controls.Add(this.Item2Button);
+            this.VendingGroup.Controls.Add(this.Item4Button);
+            this.VendingGroup.Controls.Add(this.Item3Button);
+            this.VendingGroup.Location = new System.Drawing.Point(12, 49);
+            this.VendingGroup.Name = "VendingGroup";
+            this.VendingGroup.Size = new System.Drawing.Size(82, 177);
+            this.VendingGroup.TabIndex = 9;
+            this.VendingGroup.TabStop = false;
+            this.VendingGroup.Text = "Vending";
+            this.VendingGroup.Enter += new System.EventHandler(this.VendingGroup_Enter);
+            // 
+            // PurchaseBox
+            // 
+            this.PurchaseBox.Controls.Add(this.ReturnCash);
+            this.PurchaseBox.Controls.Add(this.DollarBox);
+            this.PurchaseBox.Controls.Add(this.DimeBox);
+            this.PurchaseBox.Controls.Add(this.QuarterBox);
+            this.PurchaseBox.Controls.Add(this.NickelBox);
+            this.PurchaseBox.Location = new System.Drawing.Point(112, 49);
+            this.PurchaseBox.Name = "PurchaseBox";
+            this.PurchaseBox.Size = new System.Drawing.Size(260, 243);
+            this.PurchaseBox.TabIndex = 10;
+            this.PurchaseBox.TabStop = false;
+            this.PurchaseBox.Text = "Deposit";
+            this.PurchaseBox.Enter += new System.EventHandler(this.PurchaseBox_Enter);
+            // 
+            // ReturnCash
+            // 
+            this.ReturnCash.Location = new System.Drawing.Point(91, 214);
+            this.ReturnCash.Name = "ReturnCash";
+            this.ReturnCash.Size = new System.Drawing.Size(75, 23);
+            this.ReturnCash.TabIndex = 11;
+            this.ReturnCash.Text = "Return";
+            this.ReturnCash.UseVisualStyleBackColor = true;
+            this.ReturnCash.Click += new System.EventHandler(this.ReturnCash_Click);
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(7, 263);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(75, 23);
+            this.LoadButton.TabIndex = 11;
+            this.LoadButton.Text = "Load";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // VendingChoice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 305);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.PurchaseBox);
+            this.Controls.Add(this.VendingGroup);
+            this.Controls.Add(this.Display);
+            this.Name = "VendingChoice";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DollarBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DimeBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.QuarterBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NickelBox)).EndInit();
+            this.VendingGroup.ResumeLayout(false);
+            this.VendingGroup.PerformLayout();
+            this.PurchaseBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox NickelBox;
+        private System.Windows.Forms.PictureBox QuarterBox;
+        private System.Windows.Forms.PictureBox DimeBox;
+        private System.Windows.Forms.PictureBox DollarBox;
+        private System.Windows.Forms.RadioButton Item2Button;
+        private System.Windows.Forms.RadioButton Item1Button;
+        private System.Windows.Forms.RadioButton Item3Button;
+        private System.Windows.Forms.RadioButton Item4Button;
+        private System.Windows.Forms.TextBox Display;
+        private System.Windows.Forms.GroupBox VendingGroup;
+        private System.Windows.Forms.GroupBox PurchaseBox;
+        private System.Windows.Forms.Button ReturnCash;
+        private System.Windows.Forms.Button LoadButton;
+    }
+}
+
---
+++ VendingMachine/Form1.cs
@@ -0,0 +1,455 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace VendingMachine
+{
+    public partial class VendingChoice : Form
+    {
+        int stock;
+        double quarters;
+        double dimes;
+        double nickels;
+        double cost1;
+        double cost2;
+        double cost3;
+        double cost4;
+        string item1;
+        string item2;
+        string item3;
+        string item4;
+        int numItem1;
+        int numItem2;
+        int numItem3;
+        int numItem4;
+
+        double deposit = 0.00;
+        int countDollar = 0;
+        int countQuarter = 0;
+        int countDime = 0;
+        int countNickel = 0;
+
+        public VendingChoice()
+        {
+            InitialLoad();
+            InitializeComponent();
+        }
+
+        private void VendingGroup_Enter(object sender, EventArgs e)
+        {
+            bool soldOut = false;
+            double costItem1 = cost1;
+            double costItem2 = cost2;
+            double costItem3 = cost3;
+            double costItem4 = cost4;
+
+            if (Item1Button.Checked)
+            {
+                if (numItem1 == 0)
+                    soldOut = true;
+            }
+            else if (Item2Button.Checked)
+            {
+                if (numItem2 == 0)
+                    soldOut = true;
+            }
+            else if (Item3Button.Checked)
+            {
+                if (numItem3 == 0)
+                    soldOut = true;
+            }
+            else if (Item4Button.Checked)
+            {
+                if (numItem4 == 0)
+                    soldOut = true;
+            }
+
+            if (soldOut == false)
+            {
+                if (Item1Button.Checked)
+                {
+                    costItem1 -= deposit;
+                    if (costItem1 < 0)
+                        Display.Text = "$0.00";
+                    else
+                        Display.Text = cost1.ToString("$0.00");
+                    if (costItem1 <= 0)
+                        Dispence(costItem1, item1);
+                }
+                else if (Item2Button.Checked)
+                {
+                    costItem2 -= deposit;
+                    if (costItem2 < 0)
+                        Display.Text = "$0.00";
+                    else
+                        Display.Text = cost2.ToString("$0.00");
+                    if (costItem2 <= 0)
+                        Dispence(costItem2, item2);
+                }
+                else if (Item3Button.Checked)
+                {
+                    costItem3 -= deposit;
+                    if (costItem3 < 0)
+                        Display.Text = "$0.00";
+                    else
+                        Display.Text = cost3.ToString("$0.00");
+                    if (costItem3 <= 0)
+                        Dispence(costItem3, item3);
+                }
+                else if (Item4Button.Checked)
+                {
+                    costItem4 -= deposit;
+                    if (costItem4 < 0)
+                        Display.Text = "$0.00";
+                    else
+                        Display.Text = cost4.ToString("$0.00");
+                    if (costItem4 <= 0)
+                        Dispence(costItem4, item4);
+                }
+            }
+            else
+            {
+                Display.Text = "Sold Out";
+            }
+        }
+
+        private void PurchaseBox_Enter(object sender, EventArgs e)
+        {
+            Display.Text = deposit.ToString("$0.00");
+            Item1Button.Checked = false;
+            Item2Button.Checked = false;
+            Item3Button.Checked = false;
+            Item4Button.Checked = false;
+        }
+
+        private void DollarBox_Click(object sender, EventArgs e)
+        {
+            countDollar++;
+
+            if (countDollar == 1)
+            {
+                deposit += 1;
+                PurchaseBox_Enter(sender, e);
+            }
+            else
+            {
+                Display.Text = "Only 1 dollar bill per purchase.";
+                countDollar = 1;
+            }
+        }
+
+        private void QuarterBox_Click(object sender, EventArgs e)
+        {
+            deposit += 0.25;
+            countQuarter++;
+            PurchaseBox_Enter(sender, e);
+        }
+
+        private void DimeBox_Click(object sender, EventArgs e)
+        {
+            deposit += 0.10;
+            countDime++;
+            PurchaseBox_Enter(sender, e);
+        }
+
+        private void NickelBox_Click(object sender, EventArgs e)
+        {
+            deposit += 0.05;
+            countNickel++;
+            PurchaseBox_Enter(sender, e);
+        }
+
+        private void Item1Button_CheckedChanged(object sender, EventArgs e)
+        {
+            VendingGroup_Enter(sender, e);
+        }
+
+        private void Item2Button_CheckedChanged(object sender, EventArgs e)
+        {
+            VendingGroup_Enter(sender, e);
+        }
+
+        private void Item3Button_CheckedChanged(object sender, EventArgs e)
+        {
+            VendingGroup_Enter(sender, e);
+        }
+
+        private void Item4Button_CheckedChanged(object sender, EventArgs e)
+        {
+            VendingGroup_Enter(sender, e);
+        }
+
+        private void ReturnCash_Click(object sender, EventArgs e)
+        {
+            string phrase = "Returningin:\n";
+
+            if (countDollar == 1)
+                phrase += countDollar + " Dollar Bill.\n";
+            if (countQuarter == 1)
+                phrase += countQuarter + " Quarter.\n";
+            else if (countQuarter > 1)
+                phrase += countQuarter + " Quarters.\n";
+            if (countDime == 1)
+                phrase += countDime + " Dime.\n";
+            else if (countDime > 1)
+                phrase += countDime + " Dimes.\n";
+            if (countNickel == 1)
+                phrase += countNickel + " Nickel.\n";
+            else if (countNickel > 1)
+                phrase += countNickel + " Nickels.\n";
+
+
+            if (deposit > 0)
+            {
+                Display.Text = "Returning " + deposit.ToString("$0.00");
+                MessageBox.Show(phrase);
+                deposit = 0;
+                countDollar = 0;
+                countQuarter = 0;
+                countDime = 0;
+                countNickel = 0;
+                PurchaseBox_Enter(sender, e);
+            }
+            else
+                MessageBox.Show("No Money to return.");
+        }
+
+        private void Dispence(double cash, string item)
+        {
+            bool isChange = true;
+            double change = 0;
+            int countQ = 0;
+            int countD = 0;
+            int countN = 0;
+
+            if (Math.Round(cash, 2) == 0.00)
+            {
+                Display.Text = "Enjoy your " + item;
+                if (item == item1)
+                    numItem1--;
+                else if (item == item2)
+                    numItem2--;
+                else if (item == item3)
+                    numItem3--;
+                else if (item == item4)
+                    numItem4--;
+
+                countDollar = 0;
+                countQuarter = 0;
+                countDime = 0;
+                countNickel = 0;
+                deposit = 0;
+            }
+            else
+            {
+                while (Math.Round(cash, 2) < 0 && isChange == true)
+                {
+                    while (Math.Round(cash, 2) <= -0.25 && Math.Round(quarters, 2) != 0.00)
+                    {
+                        cash += 0.25;
+                        change += 0.25;
+                        quarters -= 0.25;
+                        countQ++;
+                    }
+                    while (Math.Round(cash, 2) <= -0.10 && Math.Round(dimes, 2) != 0.00)
+                    {
+                        cash += 0.10;
+                        change += 0.10;
+                        dimes -= 0.10;
+                        countD++;
+                    }
+                    while (Math.Round(cash, 2) <= -0.05 && Math.Round(nickels, 2) != 0.00)
+                    {
+                        cash += 0.05;
+                        change += 0.05;
+                        nickels -= 0.05;
+                        countN++;
+                    }
+                    if (Math.Round(cash, 2) != 0.00)
+                        isChange = false;
+                }
+
+                if (isChange == true)
+                {
+                    string phrase = "Change Due: " + change.ToString("$0.00") + ".\n";
+
+                    if (countQ == 1)
+                        phrase += countQ + " Quarter.\n";
+                    else if (countQ > 1)
+                        phrase += countQ + " Quarters.\n";
+                    if (countD == 1)
+                        phrase += countD + " Dime.\n";
+                    else if (countD > 1)
+                        phrase += countD + " Dimes.\n";
+                    if (countN == 1)
+                        phrase += countN + " Nickel.\n";
+                    else if (countN > 1)
+                        phrase += countN + " Nickels.\n";
+
+                    Display.Text = "Enjoy your " + item;
+                    MessageBox.Show(phrase);
+                    if (item == item1)
+                        numItem1--;
+                    else if (item == item2)
+                        numItem2--;
+                    else if (item == item3)
+                        numItem3--;
+                    else if (item == item4)
+                        numItem4--;
+
+                    deposit = 0;
+                    Display.Text = deposit.ToString("$0.00");
+                    countDollar = 0;
+                    countQuarter = 0;
+                    countDime = 0;
+                    countNickel = 0;
+                }
+                else {
+                    Display.Text = "Use exact change";
+                }
+            }
+            Item1Button.Checked = false;
+            Item2Button.Checked = false;
+            Item3Button.Checked = false;
+            Item4Button.Checked = false;
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            Item1Button.Checked = false;
+            Item2Button.Checked = false;
+            Item3Button.Checked = false;
+            Item4Button.Checked = false;
+            bool file = false;
+
+            Display.Text = deposit.ToString("$0.00");
+
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.Title = "Vending Machine Restock.";
+                ofd.Filter = "Text Files (*.txt) | *.txt | All Files(*.*) | *.*";
+                DialogResult dr = ofd.ShowDialog();
+                if (dr == DialogResult.OK)
+                {
+                    string name = ofd.FileName;
+                    StreamReader sr = new StreamReader(@name);
+                    if (new FileInfo(@name).Length == 0)
+                    {
+                        sr.Close();
+                    }
+                    else
+                    {
+                        file = true;
+                        FileLoad(name);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            if (file == false)
+            {
+                InitialLoad();
+                Item1Button.Text = item1;
+                Item2Button.Text = item2;
+                Item3Button.Text = item3;
+                Item4Button.Text = item4;
+            }
+        }
+
+        private void InitialLoad()
+        {
+            stock = 5;
+            numItem1 = stock;
+            numItem2 = stock;
+            numItem3 = stock;
+            numItem4 = stock;
+            quarters = 0.25 * stock;
+            nickels = 0.05 * stock;
+            dimes = 0.10 * stock;
+            cost1 = 0.80;
+            cost2 = 0.95;
+            cost3 = 0.60;
+            cost4 = 1.15;
+            item1 = "Peanuts";
+            item2 = "Crackers";
+            item3 = "Candy Bar";
+            item4 = "Cookies";
+        }
+
+        private void FileLoad(string name)
+        {
+            int counter = 1;
+            string line;
+            stock = 5;
+
+            quarters = 0.25 * stock;
+            nickels = 0.05 * stock;
+            dimes = 0.10 * stock;
+            numItem1 = stock;
+            numItem2 = stock;
+            numItem3 = stock;
+            numItem4 = stock;
+            StreamReader fileName = new StreamReader(@name);
+
+            while ((line = fileName.ReadLine()) != null)
+            {
+                if (counter == 1)
+                {
+                    item1 = line;
+                    counter++;
+                }
+                else if (counter == 2)
+                {
+                    cost1 = double.Parse(line);
+                    counter++;
+                }
+                else if (counter == 3)
+                {
+                    item2 = line;
+                    counter++;
+                }
+                else if (counter == 4)
+                {
+                    cost2 = double.Parse(line);
+                    counter++;
+                }
+                else if (counter == 5)
+                {
+                    item3 = line;
+                    counter++;
+                }
+                else if (counter == 6)
+                {
+                    cost3 = double.Parse(line);
+                    counter++;
+                }
+                else if (counter == 7)
+                {
+                    item4 = line;
+                    counter++;
+                }
+                else if (counter == 8)
+                {
+                    cost4 = double.Parse(line);
+                    counter++;
+                }
+            }
+            fileName.Close();
+            Item1Button.Text = item1;
+            Item2Button.Text = item2;
+            Item3Button.Text = item3;
+            Item4Button.Text = item4;
+        }
+    }
+}
