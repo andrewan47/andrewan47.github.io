9bafb4d3025c2c9a49649f9943afbb39340ae459
Andrew Alarcon <andrewan47@gmail.com>
2021-03-18 15:17:46 -0700

    Create Meterbar.cs

---
+++ Prototype Fighting Game/Meterbar.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Meterbar : MonoBehaviour
+{
+    private static Image MeterBarImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MeterBarImage = GetComponent<Image>();
+    }
+
+    public static void setMeterBarValue(int meter, int max)
+    {
+        float bar = (float)meter / max;
+        Debug.Log(bar);
+        MeterBarImage.fillAmount = bar;
+    }
+}
