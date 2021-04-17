f873ab90d058e411e65e39907ac19b15d58ce020
Andrew Alarcon <andrewan47@gmail.com>
2021-04-17 12:40:38 -0700

    Update Fireball.cs

--- Prototype Fighting Game/Fireball.cs
+++ Prototype Fighting Game/Fireball.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class Fireball : MonoBehaviour
 {
+    private int damage = 60;
+    private int chip = 0;
+    private int stun = 80;
+    private int counter = 66;
+    private int counterStun = 94;
+    private int meter = 15;
+    private int whiff = 10;
+    private int onHit = 5;
+
     //Hit detection for the fireball
     private void OnCollisionEnter(Collision collision)
     {
         GameObject opponent = collision.gameObject;
 
+        Universal opponentHealth = opponent.GetComponent<Universal>();
+
         if (opponent.name == "Opponent")
         {
-            Debug.Log("Fireball hit enemy");
+            Debug.Log(damage);
+            opponentHealth.calculateDamage(damage, onHit);
             Destroy(gameObject);
         }
     }
